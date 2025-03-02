1051f21da8a9503678595233b77e7bde0499362f
Dinusha <dinushagammanpila@gmail.com>
2025-03-02 14:09:14 +0530

    Implementation of usage of Inline Arrays

---
+++ test-dotnet-8/Models/SearchFields.cs
@@ -0,0 +1,8 @@
+﻿namespace test_dotnet_8.Models
+{
+    [System.Runtime.CompilerServices.InlineArray(3)]
+    public struct SearchFields
+    {
+        public string FieldName { get; set; }
+    }
+}
